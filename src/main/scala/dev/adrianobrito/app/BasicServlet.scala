538a156f08652a7a715f1a6dc6ca8df0ad8a4ff3
Adriano <adrianosdebrito@gmail.com>
2014-03-17 21:33:29 -0300

    Ajuste para teste do HttpClient

--- src/main/scala/dev/adrianobrito/app/BasicServlet.scala
+++ src/main/scala/dev/adrianobrito/app/BasicServlet.scala
@@ -7,5 +7,28 @@ import dev.adrianobrito.app.dao.LamentacaoDao
 
 class BasicServlet extends ScalawebtemplateStack with BasicInjector {
   
+  get("/httptest/get"){
+	  <h1> OK </h1> 
+      <p> {params} </p>
+  }
+  
+  post("/httptest/post"){
+	  <h1> OK </h1> 
+      <p>Params:  {params} </p>
+      <p>Data: {request.body} </p>
+  }
+  
+  put("/httptest/put"){
+	  <h1> OK </h1> 
+      <p>Params:  {params} </p>
+      <p>Data: {request.body} </p>
+  }
+  
+  delete("/httptest/delete"){
+	  <h1> OK </h1> 
+      <p>Params:  {params} </p>
+      <p>Data: {request.body} </p>
+  }
+  
   
 }
